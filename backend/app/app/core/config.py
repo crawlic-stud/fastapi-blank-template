77197189262ed74105e556fdac1c40e857eb15ca
crawlic-stud <nikitosik0726@gmail.com>
2023-12-19 16:05:44 +0300

    added redis and postgres conf

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -1,7 +1,9 @@
 import os
-from pydantic_settings import BaseSettings
-from pydantic import AnyHttpUrl
+from typing import Any
 from enum import Enum
+
+from pydantic_settings import BaseSettings
+from pydantic import AnyHttpUrl, PostgresDsn, field_validator, FieldValidationInfo
 
 
 class ModeEnum(str, Enum):
@@ -16,6 +18,30 @@ class Settings(BaseSettings):
     MODE: ModeEnum = ModeEnum.development
     API_VERSION: str = "v1"
     API_V1_STR: str = f"/api/{API_VERSION}"
+
+    DATABASE_USER: str
+    DATABASE_PASSWORD: str
+    DATABASE_NAME: str
+    DATABASE_HOST: str
+    DATABASE_PORT: int
+    ASYNC_DATABASE_URI: PostgresDsn | str = ""
+
+    @field_validator("ASYNC_DATABASE_URI", mode="after")
+    def assemble_db_connection(cls, v: str | None, info: FieldValidationInfo) -> Any:
+        if isinstance(v, str):
+            if v == "":
+                return PostgresDsn.build(
+                    scheme="postgresql+asyncpg",
+                    username=info.data["DATABASE_USER"],
+                    password=info.data["DATABASE_PASSWORD"],
+                    host=info.data["DATABASE_HOST"],
+                    port=info.data["DATABASE_PORT"],
+                    path=info.data["DATABASE_NAME"],
+                )
+        return v
+
+    REDIS_HOST: str
+    REDIS_PORT: str
 
     class Config:
         case_sensitive = True
